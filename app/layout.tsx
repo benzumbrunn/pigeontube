95e0c5e692140e3b188b2b83b60679870c99e886
Ben Zumbrunn <benzumbrunn@gmail.com>
2024-07-26 23:43:41 +0800

    Improve OG image

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,13 +20,13 @@ export default function RootLayout({
         <meta name="keywords" content="Pigeon Tube, DeFiChain, MetaChain, DeFi, EVM-compatible, Metamask, DFI, blockchain communication, smart contract" />
         <meta property="og:title" content="Pigeon Tube - The on-chain billboard on DeFiChain MetaChain" />
         <meta property="og:description" content="Pigeon Tube provides the DeFiChain community with a billboard on the DeFiChain MetaChain layer. Post and highlight messages using DFI via an EVM-compatible browser wallet like Metamask." />
-        <meta property="og:image" content="https://pigeon.tube/_next/static/media/icon.8515a9f1.svg" />
+        <meta property="og:image" content="https://pigeon.tube/og-image.png" />
         <meta property="og:url" content="https://www.pigeon.tube" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Pigeon Tube - The on-chain billboard on DeFiChain MetaChain" />
         <meta name="twitter:description" content="Pigeon Tube provides the DeFiChain community with a billboard on the DeFiChain MetaChain layer. Post and highlight messages using DFI via an EVM-compatible browser wallet like Metamask." />
-        <meta name="twitter:image" content="https://pigeon.tube/_next/static/media/icon.8515a9f1.svg" />
+        <meta name="twitter:image" content="https://pigeon.tube/og-image.png" />
         <meta name="robots" content="index, follow" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Pigeon Tube - The on-chain billboard on DeFiChain MetaChain</title>
